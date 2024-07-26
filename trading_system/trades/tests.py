99eebebd578028c203be6d4e1f48c3a37a2e2acd
Aminul <aminul.babor@gmail.com>
2024-07-26 22:54:48 +1000

    More test case added, update existing tests.

--- trading_system/trades/tests.py
+++ trading_system/trades/tests.py
@@ -27,12 +27,28 @@ def api_client(user):
 
 @pytest.mark.django_db
 def test_create_trade(api_client, stock):
+    assert Trade.objects.count() == 0
     response = api_client.post(
         "/api/trades/",
         {"user": 1, "stock": stock.id, "quantity": 10, "trade_type": "BUY"},
     )
     assert response.status_code == 201
     assert Trade.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_create_trade_with_unknown_stock(api_client, stock):
+    """
+    In this test, we are trying to create a trade with a stock id which does not exist
+    in our database. So, we must not allow to create a Trade and request must fail.
+    """
+    assert Trade.objects.count() == 0
+    response = api_client.post(
+        "/api/trades/",
+        {"user": 1, "stock": stock.id + 99, "quantity": 10, "trade_type": "BUY"},
+    )
+    assert response.status_code == 400
+    assert Trade.objects.count() == 0
 
 
 @pytest.mark.django_db
@@ -46,6 +62,10 @@ def test_total_value(api_client, user, stock):
 
 @pytest.mark.django_db
 def test_total_value_for_unknown_stock_id(api_client, user, stock):
+    """
+    In this test we are sending a request to get stock information which does not exist.
+    It must fail with error.
+    """
     Trade.objects.create(user=user, stock=stock, quantity=10, trade_type="BUY")
     Trade.objects.create(user=user, stock=stock, quantity=5, trade_type="BUY")
     response = api_client.get(f"/api/stocks/9999/value/")
