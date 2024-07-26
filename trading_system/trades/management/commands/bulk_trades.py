d35b88d06f53348963e43b481f6109438e536c2b
Aminul <aminul.babor@gmail.com>
2024-07-26 23:05:57 +1000

    Polishing Management command

--- trading_system/trades/management/commands/bulk_trades.py
+++ trading_system/trades/management/commands/bulk_trades.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from trades.models import Stock, Trade
 from django.core.exceptions import ObjectDoesNotExist
@@ -13,19 +13,24 @@ class Command(BaseCommand):
     help = 'Place trades in bulk from a CSV file'
 
     def handle(self, *args, **kwargs):
-        print(settings.CSV_FILE_PATH)
-        print(os.path.abspath(os.path.dirname('manage.py')))
+        # print(settings.CSV_FILE_PATH)
+        # print(os.path.abspath(os.path.dirname('manage.py')))
 
         if not settings.CSV_FILE_PATH:
-            print("CSV_FILE_PATH is not defined in the settings.py file")
+            raise CommandError("CSV_FILE_PATH is not defined in the settings.py file")
 
         with open(settings.CSV_FILE_PATH, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                user = User.objects.get(username=row['username'])
+                try:
+                    user = User.objects.get(username="test")
+                except User.DoesNotExist:
+                    error = f"User {row['username']} does not exist in our database. Quiting!"
+                    raise CommandError(error)
                 try:
                     stock = Stock.objects.get(name=row['stock'])
                 except Stock.DoesNotExist:
+                    print("Stock does not exist, creating a new stock.")
                     stock = Stock.objects.create(id=row['stock'], name=row['stock_name'], price=row['stock_price'])
                 Trade.objects.create(
                     user=user,
